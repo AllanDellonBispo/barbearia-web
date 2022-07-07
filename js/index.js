f6b64a71333798f4b16ce9bc89b5e710f40646e4
babalu4897 <lincolnramos02@gmail.com>
2022-07-06 22:26:48 -0300

    vericação de nomes e serviço

--- js/index.js
+++ js/index.js
@@ -30,7 +30,33 @@ function enviarDados(){
     servicos.map(e => {
         if(servico === e.nome){
             valorServico.style.color = 'green';
-            valorServico.innerHTML = `Preço do servico R$${e.preco.toFixed(2)}`
+            valorServico.innerHTML = `Preço do serviço R$${e.preco.toFixed(2)}`
         }
     })
+}
+
+function veerificarNome(){
+    var selecionado=nome_reserva.value
+    const regex = /^(?=.*[@!#$%^&*()/\\])[@!#$%^&*()/\\a-zA-Z0-9]{8,50}$/;
+  // alert( regex.test(selecionado));
+
+}
+
+function verificarSelecao(){
+    
+    var selecionado=document.querySelector('.serv').value
+    if(selecionado == "nenhum"){
+        alert("selecione um serviço para poder continuar")
+        event.preventDefault();
+        document.querySelector('.serv').style.border = '1px solid red';  
+       
+    }
+    
+   
+    if(!veerificarNome()){
+        alert("nome deve conter somente letras")
+        event.preventDefault();
+        document.querySelector('#nome_reserva').style.border = '1px solid red';  
+
+    }
 }
